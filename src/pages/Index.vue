466720684d5d69053344b228fa16d21b1d91ffe7
DIY <903986396@qq.com>
2017-06-11 17:04:22 +0800

    首页加入echarts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,83 +1,133 @@
 <template>
+  <el-row>
+    <!-- 导航 -->
     <el-row>
-        <!-- 导航 -->
-        <el-row>
-            <el-col :span="24" class="breadcrumb">
-                <el-breadcrumb separator="/">
-                    <el-breadcrumb-item :to="{ path: '/Index' }">系统面板</el-breadcrumb-item>
-                    <el-breadcrumb-item>系统主页</el-breadcrumb-item>
-                </el-breadcrumb>
-            </el-col>
-            <hr class="hr-line"/>
-        </el-row>
-
-        <el-row>
-            <el-col :span="24">
-                <el-table
-                        ref="multipleTable"
-                        :data="tableData3"
-                        border
-                        tooltip-effect="dark"
-                        style="width: 99%"
-                        :default-sort = "{prop: 'date', order: 'descending'}"
-                        @selection-change="handleSelectionChange">
-                    <el-table-column
-                            type="selection"
-                            width="55">
-                    </el-table-column>
-                    <el-table-column
-                            label="日期"
-                            prop="date"
-                            sortable
-                            width="120">
-                        <template scope="scope">{{ scope.row.date }}</template>
-                    </el-table-column>
-                    <el-table-column
-                            prop="name"
-                            label="姓名"
-                            sortable
-                            width="120">
-                    </el-table-column>
-                    <el-table-column
-                            prop="address"
-                            label="地址"
-                            sortable
-                            show-overflow-tooltip>
-                    </el-table-column>
-                </el-table>
-            </el-col>
-        </el-row>
+      <el-col :span="24" class="breadcrumb">
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/Index' }">系统面板</el-breadcrumb-item>
+          <el-breadcrumb-item>系统主页</el-breadcrumb-item>
+        </el-breadcrumb>
+      </el-col>
+      <hr class="hr-line"/>
     </el-row>
+    <el-row>
+      <el-col :span="11">
+        <el-card class="box-card">
+          <div id="chart-account" style="width: 100%;height: 300px"></div>
+        </el-card>
+      </el-col>
+      <el-col :span="11" offset="1">
+        <el-card class="box-card">
+          <div id="chart-visit" style="width: 100%;height: 300px"></div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </el-row>
 
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        tableData3: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
-      }
-    },
-    methods: {
-      formatter (row, column) {
-        return row.address
+    var echarts = require('echarts/lib/echarts')
+    // 引入柱状图
+    require('echarts/lib/chart/bar')
+    require('echarts/lib/chart/pie')
+    // 引入提示框和标题组件
+    require('echarts/lib/component/tooltip')
+    require('echarts/lib/component/title')
+    // 绘制图表
+
+    export default {
+      data () {
+
+      },
+      mounted () {
+        // 成交图
+        var chartAccount = echarts.init(document.getElementById('chart-account'))
+        chartAccount.setOption({
+          title: {
+            text: '成交金额',
+            left: 'center'
+          },
+          color: ['#3398DB'],
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+              type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月'],
+              axisTick: {
+                alignWithLabel: true
+              }
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ],
+          series: [
+            {
+              name: '成交金额',
+              type: 'bar',
+              barWidth: '60%',
+              data: [100000, 52, 200, 334, 3000090, 330, 220]
+            }
+          ]
+        })
+        // 饼状 来源图
+        var chartVisit = echarts.init(document.getElementById('chart-visit'))
+
+        chartVisit.setOption(
+          {
+            title: {
+              text: '某站点用户访问来源',
+              subtext: '纯属虚构',
+              x: 'center'
+            },
+            tooltip: {
+              trigger: 'item',
+              formatter: '{a} <br/>{b} : {c} ({d}%)'
+            },
+            legend: {
+              orient: 'vertical',
+              left: 'left',
+              data: [ '直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎' ]
+            },
+            series: [
+              {
+                name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                  {value: 335, name: '直接访问'},
+                  {value: 310, name: '邮件营销'},
+                  {value: 234, name: '联盟广告'},
+                  {value: 135, name: '视频广告'},
+                  {value: 1548, name: '搜索引擎'}
+                ],
+                itemStyle: {
+                  emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+                }
+              }
+            ]
+          }
+        )
       }
     }
-  }
 </script>
+
